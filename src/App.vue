8f7cce591fbb61f72ec6c8b27af079b9b1da8c86
Tabller <i02823194@gmail.com>
2025-01-12 17:14:19 +0700

    code

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@ import MainPage from './components/MainPage.vue'
 </script>
 
 <template>
-  <HelloWorld msg="Basement Studio" class="title" />
+  <MainPage msg="Basement Studio" class="title" />
 </template>
 
 <style scoped>
